4ed06b576bfe1907b5a65292b2e8a38b72f9f690
GitHubxdw <xdw215@126.com>
2016-03-13 23:31:08 +0800

    做了更改

--- ReactCocoa/ReactCocoa/AppDelegate.m
+++ ReactCocoa/ReactCocoa/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //做了修改
+    //做了修改，再次修改
     return YES;
 }
 
